140d5ce8f049478b9d593693d384b6365959d748
dalia9999 <daliabedrosyan47@gmail.com>
2025-05-16 18:25:58 +0300

    Add: products schema, routes for CRUD operations and and separate logic into controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config(); 
-import express from 'express';
-import mongoose from 'mongoose';
 
-export const connectDB = async () => {
-  try {
-    console.log('Mongo URI:', process.env.MONGO_URI);  
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected ${conn.connection.host}`);
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
-};
+import express from 'express';
+import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 const PORT = process.env.PORT || 8000;
 
@@ -20,9 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello!');
-});
+app.use('/api/products', productRoutes)
 
 const startServer = async () => {
   try {
