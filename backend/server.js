6776af97692a167f691b33557f81478a226c08f6
dalia9999 <daliabedrosyan47@gmail.com>
2025-05-17 22:38:06 +0300

    Project completed and set up for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
 
 import express from 'express';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 
@@ -9,9 +10,19 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.all('/{*any}', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 const startServer = async () => {
   try {
